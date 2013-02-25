6e0da705ae7d7057768cb5ed94f5168cdf600f7c
Matthew Hull <hull.matt@gmail.com>
2013-02-25 09:40:41 -0600

    made relative element subclass of element

--- lib/sikulio/relative_element.rb
+++ lib/sikulio/relative_element.rb
@@ -1,17 +1,14 @@
 module Sikulio
-  class RelativeElement
+  class RelativeElement < Element
 
-    attr_accessor :screen
-    attr_accessor :ident
     attr_accessor :offset_x
     attr_accessor :offset_y
 
     # Sets screen and image file path
     def initialize(screen, ident, offset_x, offset_y)
-      self.screen = screen
-      self.ident = ident
       self.offset_x = offset_x
       self.offset_y = offset_y
+      super screen, ident
     end
 
     # Click on this element
@@ -22,16 +19,5 @@ module Sikulio
 
       screen.click click_x, click_y
     end
-
-    # Find this element on screen
-    def find
-      screen.find ident
-    end
-
-    # Send keystrokes to element
-    def type(keystrokes)
-      screen.type keystrokes
-    end
-
   end
 end
